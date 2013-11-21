3fe96ababe780b6f0a45792547a069f97534129d
Eugene Rudenko <yruden@softserveinc.com>
2013-11-21 11:24:04 +0200

    start

---
+++ media/js/LogFormatter.js
@@ -0,0 +1,16 @@
+define(['lo-dash'],function(_){
+    var parameterRegExp = /{(\d+)}/g,
+        Formatter = function(template){
+            if(!template) throw new Error('template error should be supplied');
+            this.template = template;
+    }
+
+    Formatter.prototype.format = function(){
+        var args = arguments;
+        return this.template.replace(parameterRegExp, function(token, number){
+            return args[number] + '';
+        });
+    }
+
+    return Formatter;
+});
---
+++ unitTests/SpecRunnerInit.js
@@ -0,0 +1,44 @@
+require.config({
+    baseUrl:"../",
+    paths: {
+        'lo-dash': 'media/lib/lo-dash.2.3.2'
+    },
+
+    shim: {
+        'lo-dash': {
+            exports: '_'
+        }
+    }
+});
+
+require([
+    "unitTests/suits/LogFormatterSpec"
+],function() {
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmineEnv.specFilter = function(spec) {
+        return htmlReporter.specFilter(spec);
+    };
+
+    if(IsWindowLoaded) {
+        execJasmine();
+        return;
+    }
+
+    var currentWindowOnload = window.onload;
+    window.onload = function() {
+        if (currentWindowOnload) {
+            currentWindowOnload();
+        }
+        execJasmine();
+    };
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+});
